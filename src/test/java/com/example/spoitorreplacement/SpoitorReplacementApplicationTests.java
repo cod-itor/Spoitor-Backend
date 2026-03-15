87fa8ca23a5b0c9120486a6d812cee28893c6963
Dita rector <rectordita@gmail.com>
2026-03-16 01:33:11 +0700

    Changing the project to springboot Maven

---
+++ src/test/java/com/example/spoitorreplacement/SpoitorReplacementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spoitorreplacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoitorReplacementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
